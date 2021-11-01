bed2dc877b9b85f50556d640fa92c8b718110534
codebrews <aaronc024@gmail.com>
2021-10-31 22:05:35 -0400

    lot's of broken stuff to look at later

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,29 @@
 "use strict"
 
-document.getElementById('embed').addEventListener("click", embed);
+// document.getElementById('embed').addEventListener("click", embed);
 
-function embed() {
-    const url = document.getElementById('youtube').value;
-    const videoId = url.searchParams.get("v");
+// function embed() {
+//     const url = new URL(document.getElementById('url').value);
+//     const videoId = url.searchParams.get('v');
 
-    document.getElementById('video').innerHTML =
-        `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player"
-        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>`;
-}
+//     document.getElementById('video').innerHTML =
+//         `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player"
+//         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//         allowfullscreen></iframe>`;
+// }
 
-document.getElementById('set').addEventListener("click", setSpeed);
+// document.getElementById('set').addEventListener("click", setSpeed);
 
-function setSpeed() {
-    const speed = document.getElementById('speed').value;
-    document.getElementById('video').defaultPlaybackRate = speed;
-    document.getElementById('current-speed').innerHTML = speed;
-}
+// function setSpeed() {
+//     const speed = document.getElementById('speed').value;
+//     console.log(document.getElementById('video'));
+//     const video = document.getElementById('video').firstChild.firstChild.playbackRate = speed;
+//     document.getElementById('current-speed').innerHTML = speed;
+// }
 
-console.log(document.getElementById('speed').value);
+document.getElementById('upload').addEventListener('click', (event) => {
+    var file = document.getElementById('audio-file').files[0];
+    console.log(file);
+    var audio = new Audio(file);
+    document.getElementById('audio-player').setAttribute('src', file);
+});
